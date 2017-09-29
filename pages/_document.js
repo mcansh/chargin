60721af7b8501c379845bedf3f56cb3df5f31b46
Logan McAnsh <logan@mcan.sh>
2017-09-28 21:09:41 -0400

    re-add static assets

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,60 @@ class Page extends Document {
             content="initial-scale=1.0, width=device-width, viewport-fit=cover"
           />
           <link rel="manifest" href="/static/manifest.json" />
-          <meta name="description" content="a fun little project to show whether your computer is charging or not 😜" />
+          <meta
+            name="description"
+            content="a fun little project to show whether your computer is charging or not 😜"
+          />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link
+            rel="shortcut icon"
+            href="/static/favicon-32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="228x228"
+            href="/static/favicon-228.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="195x195"
+            href="/static/favicon-195.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href="/static/favicon-152.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/static/favicon-144.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="128x128"
+            href="/static/favicon-128.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href="/static/favicon-120.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="96x96"
+            href="/static/favicon-96.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href="/static/favicon-72.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            href="/static/favicon-57.png"
+          />
         </Head>
         <style jsx global>{`
           @import url('https://mcan.sh/assets/fonts/Gotham/gotham.css');
