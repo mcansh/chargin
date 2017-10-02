1811bf339687603695045ee150a2d2919237320d
Logan McAnsh <logan@mcan.sh>
2017-10-01 23:11:11 -0400

    adjust line-height

--- pages/_document.js
+++ pages/_document.js
@@ -18,55 +18,16 @@ class Page extends Document {
             content="a fun little project to show whether your computer is charging or not 😜"
           />
           <link rel="manifest" href="/static/manifest.json" />
-          <link
-            rel="shortcut icon"
-            href="/static/favicon-32.png"
-            sizes="32x32"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="228x228"
-            href="/static/favicon-228.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="195x195"
-            href="/static/favicon-195.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="152x152"
-            href="/static/favicon-152.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="144x144"
-            href="/static/favicon-144.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="128x128"
-            href="/static/favicon-128.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="120x120"
-            href="/static/favicon-120.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="96x96"
-            href="/static/favicon-96.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="72x72"
-            href="/static/favicon-72.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            href="/static/favicon-57.png"
-          />
+          <link rel="shortcut icon" href="/static/favicon-32.png" sizes="32x32" />
+          <link rel="apple-touch-icon-precomposed" sizes="228x228" href="/static/favicon-228.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="195x195" href="/static/favicon-195.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/static/favicon-152.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/favicon-144.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="128x128" href="/static/favicon-128.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/static/favicon-120.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="96x96" href="/static/favicon-96.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/favicon-72.png" />
+          <link rel="apple-touch-icon-precomposed" href="/static/favicon-57.png" />
         </Head>
         <style jsx global>{`
           @import url('https://mcan.sh/assets/fonts/Gotham/gotham.css');
@@ -88,7 +49,7 @@ class Page extends Document {
             max-width: 800px;
             width: 90%;
             margin: 0 auto;
-            line-height: 1.2;
+            line-height: 1.5;
           }
         `}</style>
         <body>
