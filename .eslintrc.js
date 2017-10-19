b927e0fdedc80334400bc8224d279464b02a1032
Logan McAnsh <logan@mcan.sh>
2017-10-19 17:35:39 -0400

    update deps/eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['airbnb', 'prettier'],
   env: {
     browser: true,
-    node: true,
   },
-  plugins: ['prettier'],
+  parser: 'babel-eslint',
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'jsx-a11y/href-no-hash': 0,
+    'react/jsx-closing-tag-location': 0,
+    'react/jsx-curly-brace-presence': 0,
     'no-unused-vars': [1, { argsIgnorePattern: 'res|next|^err' }],
     'arrow-body-style': [2, 'as-needed'],
     'no-console': 0,
